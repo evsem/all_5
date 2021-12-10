c9cb69900e68550bbbda40bb5a09dd801ffbb3e8
evsem <frukwork@gmail.com>
2021-12-10 14:39:37 +0300

    v(1)-all_5

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const App = {
+  data() {
+    return {
+      text: 'Hello VueJS',
+      placeholderText: 'Text',
+      textInputValue: '',
+      notes: [],
+    }
+  },
+  methods: {
+    //
+  },
+  computed: {
+    //
+  },
+  watch: {
+    //
+  },
+}
+
+Vue.createApp(App).mount('.container_main')
